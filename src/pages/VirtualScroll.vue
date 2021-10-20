6d858b8328730b00553e2085c588af25fcf70ffb
cinob <cinob@foxmail.com>
2021-10-20 18:40:26 +0800

    ✨ 虚拟滚动

---
+++ src/pages/VirtualScroll.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="scroll-container" ref="container">
+    <div class="scroll-content" :style="`height:${contentHeight}px`">
+      <div class="translate" :style="`transform:translateY(${translateY}px)`">
+        <div class="item" v-for="item in list"
+        >
+          元素{{ item.i }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted, computed, nextTick } from 'vue'
+
+// 模拟数据
+const data = ref(Array.from({length:200}))
+// 滚动容器
+const container = ref(null)
+// 滚动容器高度
+const containerHeight = ref(0)
+// 列表项高度
+const itemHeight = ref(30)
+const itemHeightPx = computed(() => itemHeight.value + 'px')
+// 列表总高度
+const contentHeight = computed(() => itemHeight.value * data.value.length)
+// 渲染列表偏移量
+const translateY = ref(0)
+// 可视区渲染数量
+const itemNum = computed(() => Math.ceil(containerHeight.value / itemHeight.value))
+// 可视区前后补白
+const pad = ref(200)
+// 渲染节点总数 前补白 + 可视区 + 后补白
+const num = computed(() => itemNum.value)
+// 渲染节点开始索引
+const startIndex = computed(() => {
+  return translateY.value
+    ? ((translateY.value / itemHeight.value) | 0) - 1
+    : 0
+})
+// 渲染节点列表
+const list = computed(() => {
+  return data.value.slice(startIndex.value, startIndex.value + num.value).map((item, index) => ({i: startIndex.value + index}))
+})
+
+
+// 节流函数
+function debounce (func, delay = 200) {
+  let timer = null
+  return (...params) => {
+    timer && clearTimeout(timer)
+    timer = setTimeout(() => func(...params), delay)
+  }
+}
+
+onMounted(() => {
+  containerHeight.value = container.value.getBoundingClientRect().height
+  container.value.addEventListener('scroll', debounce((e) => {
+    translateY.value = e.target.scrollTop
+  }, 10))
+})
+
+</script>
+
+<style scoped lang="stylus">
+.scroll-container
+  margin-top 100px
+  height 400px
+  overflow-y auto
+  border 1px solid #123
+.item
+  width 100%
+  height v-bind('itemHeightPx')
+</style>
